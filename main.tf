ecfd46db78bbcce01d91ff6c9fba0d7e647ae10a
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-02-24 09:29:23 +0100

    Remove weird chars

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-module "dicom" {​​
+module "dicom" {
   source  = "philips-labs/dicom/hsdp"
   version = "0.0.4"
   # insert the 3 required variables here
   cdr_base_url           = "https://cdr-dev.us-east.philips-healthsuite.com"
   dicom_store_config_url = "https://dss-config-hsdpeng-dev.us-east.philips-healthsuite.com"
   iam_org_id             = "26e584e2-4147-42c0-aad9-3c70650570f1"
-}​​
+}
